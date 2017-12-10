1bb79ba940a737853277021104de6998eb0bda80
Sergey <sunliveua@gmail.com>
2017-12-10 15:57:33 +0200

    make it better

--- server/src/main.js
+++ server/src/main.js
@@ -1,3 +1,15 @@
-require('babel-register');
-require('./server');
+import express from 'express';
+import cors from 'cors';
 
+const app = express();
+const port = process.env.PORT || 8081;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => console.log('Server is on at port ', port));
+
+app.get('/', (req, res) => {
+  res.send('hello there');
+});
--- server/src/server.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import { each } from 'lodash/each';
-
-const app = express();
-
