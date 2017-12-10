4a27d227a4eba9a539ccfdb531f558a029c5be19
Sergey <sunliveua@gmail.com>
2017-12-10 14:04:07 +0200

    make it better

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => console.log('Server is on'));
