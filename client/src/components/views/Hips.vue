bc3c8716c506b959c10cc9ce6cb5b2a8ea21c202
Sergey <sunliveua@gmail.com>
2017-12-11 16:32:33 +0200

    make it better

--- client/src/components/views/Hips.vue
+++ client/src/components/views/Hips.vue
@@ -16,6 +16,43 @@
   </v-container>
 </template>
 
-<script src="./Hips"></script>
+<script>
+export default {
+  data() {
+    return {
+      hips: null,
+    };
+  },
+  computed: {
+    getHips() {
+      return this.hips;
+    },
+    getHipsSize() {
+      const params = {
+        size: null,
+        number: null,
+      };
+      if (this.getHips >= 90 && this.getHips < 94) {
+        params.size = 'XS';
+        params.number = this.getHips;
+      }
+      if (this.getHips > 93 && this.getHips < 98) {
+        params.size = 'S';
+        params.number = this.getHips;
+      }
+      if (this.getHips > 94 && this.getHips < 102) {
+        params.size = 'M';
+        params.number = this.getHips;
+      }
+      if (this.getHips > 101 && this.getHips <= 105) {
+        params.size = 'L';
+        params.number = this.getHips;
+      }
+      this.$emit('setHips', params);
+      return params.size;
+    },
+  },
+};
+</script>
 
 <style scoped lang="stylus"></style>
