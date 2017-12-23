4c7279472b41673620e54032ae6c87bb4215c8a4
Sergey <sunliveua@gmail.com>
2017-12-23 18:08:20 +0200

    🙈 🙉 🙊

--- src/components/views/Waist.vue
+++ src/components/views/Waist.vue
@@ -19,7 +19,7 @@ export default {
   methods: {
     onSave() {
       this.$store.commit('setWaist', this.getWaist);
-      this.$router.push('/');
+      this.$router.push('/params');
     },
   },
   computed: {
