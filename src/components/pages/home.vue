a0c9cb28c1628500a5bd138ad14b1dc73ba20d9c
Sergey <sunliveua@gmail.com>
2017-12-27 22:34:46 +0200

    make it better

---
+++ src/components/pages/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid>
+    <v-layout justify-center align-center>
+      <v-flex xs12 sm6 class="text-xs-center">
+        <h1>Добро пожаловать в онлайн примерочную</h1>
+        <p>Здесь вы можете примерить на себе конкретную вещь или же все вещи из нашей базы.</p>
+      </v-flex>
+    </v-layout>
+    <v-layout justify-center>
+      <v-flex xs12 sm6 class="text-xs-center">
+        <v-btn
+        color="primary"
+        to="/params"
+        >Начать</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
---
+++ src/components/pages/result.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container id="container" fluid grid-list-xl>
+    <v-layout justify-center align-center row wrap v-if="!isLoading">
+      <v-flex xs12 sm6 md4 v-for="(item, i) in filtered" :key="i">
+        <v-card>
+          <v-card-media height="350" :src="item.src"></v-card-media>
+          <v-card-text>
+            <div>Коэффициент {{ item.percent }} %</div>
+            <div class="mb-2">{{ item.title }}</div>
+            <div>Ваш предпочитаемый размер: <strong>{{ item.size }}</strong></div>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn :href="`${item.link}?attribute_pa_size=${item.size}`" target="_blank">Посмотреть</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <AppLoader v-if="isLoading" />
+  </v-container>
+</template>
+
+<script>
+export default {
+  computed: {
+    filtered() {
+      return this.$store.getters.filtered;
+    },
+    isLoading() {
+      return this.$store.getters.isLoading;
+    },
+  },
+  created() {
+    setTimeout(() => {
+      this.$store.commit('setLoading', false);
+    }, 1000);
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.card__text
+  min-height: 200px !important;
+
+#container
+  max-width 1280px
+  margin 0 auto
+</style>
+
