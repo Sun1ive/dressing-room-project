35facc6b1af4b2bde8f6175fa862418dbbb8593d
Sergey <sunliveua@gmail.com>
2017-12-27 14:19:19 +0200

    make it better

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,7 +33,7 @@ module.exports = {
     ],
     'linebreak-style': 0,
     'no-param-reassign': [2, { props: false }],
-    'no-underscore-dangle': [2, { props: false }],
+    'no-underscore-dangle': 0,
     // allow optionalDependencies
     'import/no-extraneous-dependencies': [
       'error',
