debed911667e388b27884f34485eb210134390bb
Sergey <sunliveua@gmail.com>
2017-12-27 22:31:14 +0200

    make it better

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,94 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+
+import AdminCreate from '@/components/admin/AdminCreate';
+import AdminEdit from '@/components/admin/AdminEdit';
+import AdminView from '@/components/admin/AdminView';
+
+import SignIn from '@/components/user/SignIn';
+
+import Params from '@/components/pages/Params';
+import Home from '@/components/pages/Home';
+
+import Breast from '@/components/userParams/Breast';
+import Waist from '@/components/userParams/Waist';
+import Hips from '@/components/userParams/Hips';
+import Arm from '@/components/userParams/Arm';
+
+import Result from '@/components/pages/Result';
+import singleResult from '@/components/pages/singleResult';
+import Insert from '@/components/pages/Insert';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
+      path: '/signin',
+      name: 'SignIn',
+      component: SignIn,
+    },
+    {
+      path: '/admin/create',
+      name: 'adminCreate',
+      component: AdminCreate,
+    },
+    {
+      path: '/admin/view',
+      name: 'adminView',
+      component: AdminView,
+    },
+    {
+      path: '/admin/Edit/:id',
+      name: 'adminEdit',
+      props: true,
+      component: AdminEdit,
+    },
+    {
+      path: '/insert',
+      name: 'Insert',
+      component: Insert,
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/params',
+      name: 'Params',
+      component: Params,
+    },
+    {
+      path: '/breast',
+      name: 'Breast',
+      component: Breast,
+    },
+    {
+      path: '/waist',
+      name: 'Waist',
+      component: Waist,
+    },
+    {
+      path: '/hips',
+      name: 'Hips',
+      component: Hips,
+    },
+    {
+      path: '/arm',
+      name: 'Arm',
+      component: Arm,
+    },
+    {
+      path: '/result',
+      name: 'Result',
+      component: Result,
+    },
+    {
+      path: '/single-result',
+      name: 'SingleResult',
+      component: singleResult,
     },
   ],
 });
---
+++ src/store/index.js
@@ -0,0 +1,99 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { withAuth, withOutAuth } from '../services/api';
+import compare from '../utils/compare';
+import { SessionStorage } from '../utils/storage';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    items: null,
+    breast: null,
+    waist: null,
+    hips: null,
+    arm: null,
+    filtered: null,
+    selectedItem: null,
+    loading: false,
+    error: false,
+    isUserSignIn: false,
+  },
+  mutations: {
+    setLoadedDresses(state, payload) {
+      state.items = payload;
+    },
+    setSelectedItem(state, payload) {
+      state.selectedItem = payload;
+    },
+    setBreast(state, payload) {
+      state.breast = payload;
+    },
+    setWaist(state, payload) {
+      state.waist = payload;
+    },
+    setHips(state, payload) {
+      state.hips = payload;
+    },
+    setArm(state, payload) {
+      state.arm = payload;
+    },
+    setFilteredDresses(state, payload) {
+      state.filtered = payload;
+    },
+    setLoading(state, payload) {
+      state.loading = payload;
+    },
+    setUserSignIn(state, payload) {
+      state.isUserSignIn = payload;
+    },
+    runCompare(state, payload) {
+      const result = compare(payload, state.breast, state.waist, state.hips);
+      state.filtered = result;
+    },
+  },
+  actions: {
+    getDresses({ commit }) {
+      async function fetchDresses() {
+        try {
+          const response = await withOutAuth().get('/products');
+          const resolved = response.data;
+          commit('setLoadedDresses', resolved);
+        } catch (error) {
+          throw new Error(`Could not fetch data ${error}`);
+        }
+      }
+      fetchDresses();
+    },
+    onSignIn({ commit }, payload) {
+      async function onLogIn() {
+        try {
+          const response = await withAuth(payload.username, payload.password).get('/login');
+          if (response.status === 200) {
+            const credentials = {
+              username: payload.username,
+              password: payload.password,
+            };
+            SessionStorage.set('userAdminCredentials', credentials);
+            commit('setUserSignIn', true);
+          }
+        } catch (error) {
+          throw new Error(`Wrong login or password ${error}`);
+        }
+      }
+      onLogIn();
+    },
+  },
+  getters: {
+    items: (state) => state.items,
+    getBreast: (state) => state.breast,
+    getWaist: (state) => state.waist,
+    getHips: (state) => state.hips,
+    getArm: (state) => state.arm,
+    filtered: (state) => state.filtered,
+    selectedItem: (state) => state.selectedItem,
+    isLoading: (state) => state.loading,
+    isError: (state) => state.error,
+    isUserSignIn: (state) => state.isUserSignIn,
+  },
+});
