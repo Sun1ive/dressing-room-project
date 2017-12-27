2b549ef285b8bf96fc08b5516f44ed27152cd441
Sergey <sunliveua@gmail.com>
2017-12-27 22:33:46 +0200

    make it better

---
+++ src/components/templates/myContaner.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container :fluid="fluid">
+    <v-layout :align-center="alignCenter" :justify-center="justifyCenter">
+      <v-flex xs12 sm6 lg3 class="text-xs-center">
+        <slot></slot>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    fluid: {
+      type: Boolean,
+      default: false,
+    },
+    alignCenter: {
+      type: Boolean,
+      default: false,
+    },
+    justifyCenter: {
+      type: Boolean,
+      default: false,
+    },
+    row: {
+      type: Boolean,
+      default: false,
+    },
+    wrap: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
---
+++ src/components/user/signIn.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container fluid>
+    <v-layout justify-center align-center>
+      <v-flex xs12 sm6 lg4>
+        <v-form @submit.prevent="onLogIn">
+          <v-text-field required v-model="credentials.username" label="Username" />
+          <v-text-field required v-model.number="credentials.password" label="Password" type="password" />
+          <v-btn type="submit">Sign in</v-btn>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      credentials: {
+        username: '',
+        password: null,
+      },
+    };
+  },
+  methods: {
+    onLogIn() {
+      this.$store.dispatch('onSignIn', this.credentials);
+      setTimeout(() => {
+        if (this.$store.getters.isUserSignIn) {
+          this.$router.push('/');
+        }
+      }, 500);
+    },
+  },
+};
+</script>
+
