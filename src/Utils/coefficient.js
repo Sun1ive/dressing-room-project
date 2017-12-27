383a108bfd02cda1e8c5c468d90a0196be04645a
Sergey <sunliveua@gmail.com>
2017-12-27 21:36:31 +0200

    'test'

--- src/Utils/coefficient.js
+++
@@ -1,4 +0,0 @@
-// @flow
-
-export default (userBreast, itemBreast, userWaist, itemWaist, userHips, itemHips) =>
-  Math.floor(userBreast / itemBreast * (userWaist / itemWaist) * (userHips / itemHips) * 100);
