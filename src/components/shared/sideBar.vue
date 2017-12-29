0894801cae4cdf671bb53e70536e6f5b7acfe317
Sergey <sunliveua@gmail.com>
2017-12-29 15:19:55 +0200

    make it better

---
+++ src/components/shared/sideBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-navigation-drawer
+      app
+      dark
+      v-model="drawer"
+      absolute
+      temporary
+      clipped
+    >
+      <v-list>
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title" 
+           @click="$router.push(`${item.path}`)"
+        >
+          <v-list-tile-action>
+            <v-icon dark>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="drawer = !drawer">
+          <v-list-tile-action>
+            <v-icon dark>close</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            Свернуть
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/shared/toolbar.vue
+++ src/components/shared/toolbar.vue
@@ -1,36 +1,6 @@
 <template>
   <nav>
-    <v-navigation-drawer
-      app
-      dark
-      v-model="drawer"
-      absolute
-      temporary
-      clipped
-    >
-      <v-list>
-        <v-list-tile
-          v-for="item in menuItems"
-          :key="item.title"
-          @click="$router.push(`${item.path}`)"
-        >
-          <v-list-tile-action>
-            <v-icon dark>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile @click="drawer = !drawer">
-          <v-list-tile-action>
-            <v-icon dark>close</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            Свернуть
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+    <app-sidebar></app-sidebar>
     <v-toolbar dark color="primary">
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn
@@ -75,7 +45,12 @@
 </template>
 
 <script>
+import sideBar from './sideBar';
+
 export default {
+  components: {
+    'app-sidebar': sideBar,
+  },
   data() {
     return {
       drawer: false,
