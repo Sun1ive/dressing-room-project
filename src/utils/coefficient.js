cc96f56002c0103cc27dfc1ca4be42aed96af09c
Sergey <sunliveua@gmail.com>
2018-01-01 16:06:07 +0200

    make it better

--- src/utils/coefficient.js
+++ src/utils/coefficient.js
@@ -1,2 +1,23 @@
 export default (userBreast, itemBreast, userWaist, itemWaist, userHips, itemHips) =>
-  Math.floor(userBreast / itemBreast * (userWaist / itemWaist) * (userHips / itemHips) * 100);
+    Math.floor(userBreast / itemBreast * (userWaist / itemWaist) * (userHips / itemHips) * 100);
+
+// export const upper = (
+//   userShoulders,
+//   itemShoulders,
+//   userBreast,
+//   itemBreast,
+//   userWaist,
+//   itemWaist,
+//   userHips,
+//   itemHips,
+// ) => Math.floor(
+//     (userShoulders / itemShoulders) *
+//       (userBreast / itemBreast) *
+//       (userWaist / itemWaist) *
+//       (userHips / itemHips) *
+//       100,
+//   );
+
+
+// export const bottom = (userWaist, itemWaist, userHips, itemHips) => 
+//   Math.floor((userWaist / itemWaist) * (userHips / itemHips) * 100);
