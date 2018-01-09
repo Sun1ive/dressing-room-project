72bbbfaec917b8f845fde6fe3ffd47f9631f4142
Sergey <sunliveua@gmail.com>
2018-01-09 13:10:25 +0200

    make it better

---
+++ src/components/Pages/Insert.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-layout align-center justify-center>
+      <v-flex xs10 sm8 md6 lg4 class="text-xs-center">
+        <v-form @submit.prevent="onCheckout">
+          <h1>Вставьте ссылку на товар</h1>
+          <v-text-field
+          label="Вставьте ссылку"
+          v-model.lazy="link"
+          ></v-text-field>
+          <v-btn
+          color="primary"
+          type="submit"
+          >Посмотреть</v-btn>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      link: ''
+    }
+  },
+  methods: {
+    onCheckout() {
+      console.log(123);
+    }
+  }
+}
+</script>
---
+++ src/components/User/State/SignIn.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container fluid>
+    <v-layout justify-center align-center>
+      <v-flex xs12 sm6 lg4>
+        <v-alert
+          v-if="isError.length > 0"
+          color="error" 
+          icon="warning" 
+          value="true"
+        >
+          {{ isError }}
+        </v-alert>
+      </v-flex>
+    </v-layout>
+    <v-layout justify-center align-center>
+      <v-flex xs12 sm6 lg4>
+        <v-form @submit.prevent="onLogIn">
+          <v-text-field 
+            required 
+            v-model.lazy="credentials.email" 
+            label="email" 
+          />
+          <v-text-field 
+            required 
+            v-model.number.lazy="credentials.password" 
+            label="Password" 
+            type="password" 
+          />
+          <v-btn type="submit">Sign in</v-btn>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  data() {
+    return {
+      credentials: {
+        email: '',
+        password: null,
+      },
+    };
+  },
+  methods: {
+    onLogIn() {
+      this.$store.dispatch('onSignIn', this.credentials);
+      /* Надо переписать это. вместо таймаута сделать лоадер мб */
+      setTimeout(() => {
+        if (this.isUserLoginState) {
+          this.$router.push('/');
+        }
+      }, 500);
+    },
+  },
+  computed: {
+    ...mapGetters({
+      isError: 'isError',
+      isUserLoginState: 'isUserLoginState',
+    }),
+  },
+};
+</script>
+
