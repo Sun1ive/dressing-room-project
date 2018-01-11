f0fccacfc8df3cb6061384c6a30bbc15673b9e5e
Sergey <sunliveua@gmail.com>
2018-01-11 16:18:54 +0200

    make it better

--- src/store/index.js
+++ src/store/index.js
@@ -44,7 +44,7 @@ export default new Vuex.Store({
       state.itemType = payload;
     },
     setSelectedItem(state, payload) {
-      state.selectedItem = payload;
+      state.selectedItem = payload.trim();
     },
     setUserParams(state, payload) {
       switch (payload.name) {
