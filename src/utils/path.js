5a18973724f2598e6d32381c23dd2f9ace5bd4d9
Sergey <sunliveua@gmail.com>
2018-01-12 23:52:11 +0200

    make it better

--- src/utils/path.js
+++ src/utils/path.js
@@ -1,15 +1,17 @@
 const reg1 = /(%2F)/g;
 const reg2 = /(%3A)/g;
+const reg3 = /(%20)/g;
 
 export default path => {
-  const one = path.replace(reg1, '/');
-  const two = one.replace(reg2, ':');
+  let str = path.replace(reg1, '/');
+  str = str.replace(reg2, ':');
+  str = str.replace(reg3, '');
   let result;
 
-  if (two.indexOf('?') === -1) {
+  if (str.indexOf('?') === -1) {
     result = null;
   } else {
-    result = two.slice(two.indexOf('?') + 1);
+    result = str.slice(str.indexOf('?') + 1);
   }
   return result;
 };
