4691217218aa2f0dc95dafdc361c10c199f677c9
Sergey <sunliveua@gmail.com>
2018-01-12 23:31:40 +0200

    make it better

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,7 +3,7 @@ import { SessionStorage } from '../utils/storage';
 
 export default {
   getItems({ commit }) {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
       async function fetchData() {
         try {
           const response = await withOutAuth().get('/products');
@@ -11,7 +11,7 @@ export default {
           commit('setItems', data);
           resolve();
         } catch (error) {
-          reject(Error('Its failed'));
+          // reject(Error('Its failed'));
           throw new Error(`Could not fetch data ${error.response.data}`);
         }
       }
@@ -27,8 +27,8 @@ export default {
     });
   },
   compareSingle({ commit, dispatch, state }, payload) {
-    commit('setErrorMessage', '');
-    commit('setErrorState', false);
+    // commit('setErrorMessage', '');
+    // commit('setErrorState', false);
     return new Promise(resolve => {
       async function getItem() {
         try {
@@ -36,18 +36,18 @@ export default {
             link: payload,
             params: state.userParams,
           });
-          if (state.isErrorState) {
-            commit('setErrorState', false);
-          }
+          // if (state.isErrorState) {
+          //   commit('setErrorState', false);
+          // }
           commit('setItems', response.data);
           resolve();
         } catch (error) {
-          commit('setErrorMessage', error.response.data.message);
+          // commit('setErrorMessage', error.response.data.message);
           dispatch('sendMail', {
             mailType: '404',
             link: payload,
           });
-          commit('setErrorState', true);
+          // commit('setErrorState', true);
           throw new Error(error);
         }
       }
@@ -55,7 +55,7 @@ export default {
     });
   },
   compareProductsWithType({ commit, state }) {
-    commit('setLoading', true);
+    // commit('setLoading', true);
     return new Promise(resolve => {
       async function compareAll() {
         try {
@@ -63,41 +63,22 @@ export default {
             type: state.itemType,
             params: state.userParams,
           });
-          if (state.isErrorState) {
-            commit('setErrorState', false);
-          }
+          // if (state.isErrorState) {
+          //   commit('setErrorState', false);
+          // }
           commit('setItems', response.data);
           resolve();
         } catch (error) {
-          commit('setErrorMessage', error.response.data.message);
-          commit('setErrorState', true);
+          // commit('setErrorMessage', error.response.data.message);
+          // commit('setErrorState', true);
           throw new Error(error);
         }
       }
       compareAll();
     });
   },
-  compareProductsWithTypeAndColor({ commit, state }, payload) {
-    async function compareByTypeAndColor() {
-      try {
-        const response = await withOutAuth().post('/products/all/color', {
-          type: state.itemType,
-          params: state.userParams,
-          color: payload,
-        });
-        if (state.isErrorState) {
-          commit('setErrorState', false);
-        }
-        commit('setItems', response.data);
-      } catch (error) {
-        commit('setErrorState', true);
-        commit('setErrorMessage', error.response.data);
-      }
-    }
-    compareByTypeAndColor();
-  },
-  onSignIn({ commit, state }, payload) {
-    return new Promise((resolve, reject) => {
+  onSignIn({ commit }, payload) {
+    return new Promise(resolve => {
       async function onLogIn() {
         try {
           const response = await withOutAuth().post('/user/login', {
@@ -107,16 +88,15 @@ export default {
           const { token } = response.data;
           SessionStorage.set('AuthToken', token);
 
-          if (state.error) {
-            commit('setErrorState', false);
-            commit('setErrorMessage', '');
-          }
+          // if (state.error) {
+          //   commit('setErrorState', false);
+          //   commit('setErrorMessage', '');
+          // }
           commit('setUserLoginState', true);
           resolve();
         } catch (error) {
-          commit('setErrorMessage', error.response.data.message);
-          commit('setErrorState', true);
-          reject(Error('Something went wrong'));
+          // commit('setErrorMessage', error.response.data.message);
+          // commit('setErrorState', true);
           throw new Error(error);
         }
       }
