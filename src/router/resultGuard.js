858d9e15551b3084e3b37bac42a010a536800e68
sun1ive <sunliveua@gmail.com>
2018-01-15 13:22:16 +0200

    fix

--- src/router/resultGuard.js
+++ src/router/resultGuard.js
@@ -1,6 +1,7 @@
 import store from '../store';
 
 export default (to, from, next) => {
+  store.dispatch('compareProductsWithType');
   if (store.getters.items.length < 1) {
     next('/404');
   } else {
