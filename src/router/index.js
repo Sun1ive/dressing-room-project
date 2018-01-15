ca2393d1e31c5d3a2ad43b881b1d8bfcf03db509
Sergey <sunliveua@gmail.com>
2018-01-15 22:37:01 +0200

    make it better

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-// import Home from '@/components/Pages/Home';
-/* import Insert from '@/components/Pages/Insert';
-import Result from '@/components/Pages/Result';
-import SignIn from '@/components/User/State/SignIn';
-import ErrorPage from '@/components/Pages/404';
-
-import Height from '@/components/User/Params/Height';
-import Shoulders from '@/components/User/Params/Shoulders';
-import Breast from '@/components/User/Params/Breast';
-import Waist from '@/components/User/Params/Waist';
-import Hips from '@/components/User/Params/Hips';
-
-import Admin from '@/components/Admin/Admin';
-import AdminCreate from '@/components/Admin/AdminCreate';
-import AdminView from '@/components/Admin/AdminView';
-import AdminEdit from '@/components/Admin/AdminEdit'; */
-
+import * as LazyLoad from './lazyLoad';
 import beforeAdmin from './adminGuard';
 import beforeResult from './resultGuard';
 
@@ -29,74 +13,74 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: () => import('@/components/Pages/Home'),
-    },
-    {
-      path: '/signin',
-      name: 'SignIn',
-      component: () => import('@/components/User/State/SignIn'),
+      component: LazyLoad.Home,
     },
     {
       path: '/insert',
       name: 'Insert',
-      component: () => import('@/components/Pages/Insert'),
+      component: LazyLoad.Insert,
+    },
+    {
+      path: '/signin',
+      name: 'SignIn',
+      component: LazyLoad.SignIn,
     },
     {
       path: '/shoulders',
       name: 'Shoulders',
-      component: () => import('@/components/User/Params/Shoulders'),
+      component: LazyLoad.Shoulders,
     },
     {
       path: '/breast',
       name: 'Breast',
-      component: () => import('@/components/User/Params/Breast'),
+      component: LazyLoad.Breast,
     },
     {
       path: '/height',
       name: 'Height',
-      component: () => import('@/components/User/Params/Height'),
+      component: LazyLoad.Height,
     },
     {
       path: '/waist',
       name: 'Waist',
-      component: () => import('@/components/User/Params/Waist'),
+      component: LazyLoad.Waist,
     },
     {
       path: '/hips',
       name: 'Hips',
-      component: () => import('@/components/User/Params/Hips'),
+      component: LazyLoad.Hips,
     },
     {
       path: '/404',
       name: 'FourOFour',
-      component: () => import('@/components/Pages/404'),
+      component: LazyLoad.FourOFour,
     },
     {
       path: '/result',
       name: 'Result',
-      component: () => import('@/components/Pages/Result'),
+      component: LazyLoad.Result,
       beforeEnter: beforeResult,
     },
     {
       path: '/admin',
       name: 'Admin',
-      component: () => import('@/components/Admin/Admin'),
       beforeEnter: beforeAdmin,
+      component: LazyLoad.Admin,
       children: [
         {
           path: 'view',
           name: 'adminView',
-          component: () => import('@/components/Admin/AdminView'),
+          component: LazyLoad.AdminView,
         },
         {
           path: 'create',
           name: 'adminCreate',
-          component: () => import('@/components/Admin/AdminCreate'),
+          component: LazyLoad.AdminCreate,
         },
         {
           path: 'edit/:id',
           name: 'adminEdit',
-          component: () => import('@/components/Admin/AdminEdit'),
+          component: LazyLoad.AdminEdit,
           props: true,
         },
       ],
---
+++ src/router/lazyLoad.js
@@ -0,0 +1,16 @@
+export const Home = () => import(/* webpackChunkName: 'group-pages' */ '@/components/Pages/Home');
+export const Insert = () => import(/* webpackChunkName: 'group-pages' */ '@/components/Pages/Insert');
+export const Result = () => import(/* webpackChunkName: 'group-pages' */ '@/components/Pages/Result');
+export const FourOFour = () => import(/* webpackChunkName: 'group-pages' */ '@/components/Pages/404');
+ 
+export const Admin = () => import(/* webpackChunkName: 'group-admin' */ '@/components/Admin/Admin');
+export const AdminView = () => import(/* webpackChunkName: 'group-admin' */ '@/components/Admin/AdminView');
+export const AdminCreate = () => import(/* webpackChunkName: 'group-admin' */ '@/components/Admin/AdminCreate');
+export const AdminEdit = () => import(/* webpackChunkName: 'group-admin' */ '@/components/Admin/AdminEdit');
+
+export const Shoulders = () => import(/* webpackChunkName: 'group-user' */ '@/components/User/Params/Shoulders');
+export const Breast = () => import(/* webpackChunkName: 'group-user' */ '@/components/User/Params/Breast');
+export const Waist = () => import(/* webpackChunkName: 'group-user' */ '@/components/User/Params/Waist');
+export const Hips = () => import(/* webpackChunkName: 'group-user' */ '@/components/User/Params/Hips');
+export const Height = () => import(/* webpackChunkName: 'group-user' */ '@/components/User/Params/Height');
+export const SignIn = () => import(/* webpackChunkName: 'group-user' */ '@/components/User/State/SignIn');
