9924a50c9708cd1875e1f388f534d1e667800b53
sun1ive <sunliveua@gmail.com>
2018-01-22 13:31:07 +0200

    GG

--- src/components/Shared/Loader.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-layout justify-center align-center>
-    <v-progress-circular indeterminate :size="70" color="primary" />
-  </v-layout>
-</template>
--- src/router/adminGuard.js
+++
@@ -1,8 +0,0 @@
-import store from '../store';
-
-export default async (to, from, next) => {
-  store.commit('setLoading', true);
-  await store.dispatch('getItems');
-  store.commit('setLoading', false);
-  next();
-};
